b8b7d41821267818b2ba1ea7a88424e1b99dac73
aedelfrid <almck51@gmail.com>
2023-09-24 18:43:03 -0300

    added seeding and seeds

---
+++ utils/seeds/seedData.js
@@ -0,0 +1,47 @@
+module.exports = {
+    userSeeds: [
+        {
+            username: "ROvELEsA",
+            email: "ROvELEsA@gmail.com"
+        },
+        {
+            username: "arctiste",
+            email: "arctiste@gmail.com"
+        },
+        {
+            username: "ritabley",
+            email: "ritableyA@gmail.com"
+        },
+        {
+            username: "Floromancy",
+            email: "Floromancy@gmail.com"
+        },
+        {
+            username: "Mondayitis",
+            email: "Mondayitis@gmail.com"
+        }
+    ],
+
+    thoughtSeeds: [
+        {
+            thoughtText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fringilla tellus vehicula enim ullamcorper, at sodales lorem facilisis. Nullam placerat sapien et quam laoreet iaculis. Aliquam ullamcorper lorem ante, non mattis tortor sagittis eu. Aliquam ac tempor nibh. Vestibulum sit amet laoreet arcu. Aenean ut diam eget nunc finibus cursus. Pellentesque rutrum dapibus pellentesque.",
+            username: "ROvELEsA"
+        },
+        {
+            thoughtText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fringilla tellus vehicula enim ullamcorper, at sodales lorem facilisis. Nullam placerat sapien et quam laoreet iaculis. Aliquam ullamcorper lorem ante, non mattis tortor sagittis eu. Aliquam ac tempor nibh. Vestibulum sit amet laoreet arcu. Aenean ut diam eget nunc finibus cursus. Pellentesque rutrum dapibus pellentesque.",
+            username: "arctiste"
+        },
+        {
+            thoughtText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fringilla tellus vehicula enim ullamcorper, at sodales lorem facilisis. Nullam placerat sapien et quam laoreet iaculis. Aliquam ullamcorper lorem ante, non mattis tortor sagittis eu. Aliquam ac tempor nibh. Vestibulum sit amet laoreet arcu. Aenean ut diam eget nunc finibus cursus. Pellentesque rutrum dapibus pellentesque.",
+            username: "ritabley"
+        },
+        {
+            thoughtText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fringilla tellus vehicula enim ullamcorper, at sodales lorem facilisis. Nullam placerat sapien et quam laoreet iaculis. Aliquam ullamcorper lorem ante, non mattis tortor sagittis eu. Aliquam ac tempor nibh. Vestibulum sit amet laoreet arcu. Aenean ut diam eget nunc finibus cursus. Pellentesque rutrum dapibus pellentesque.",
+            username: "Floromancy"
+        },
+        {
+            thoughtText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fringilla tellus vehicula enim ullamcorper, at sodales lorem facilisis. Nullam placerat sapien et quam laoreet iaculis. Aliquam ullamcorper lorem ante, non mattis tortor sagittis eu. Aliquam ac tempor nibh. Vestibulum sit amet laoreet arcu. Aenean ut diam eget nunc finibus cursus. Pellentesque rutrum dapibus pellentesque.",
+            username: "Mondayitis"
+        }
+    ]
+}
